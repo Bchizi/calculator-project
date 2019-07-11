f3c7450604dc75b6c457778affa2fa78a0f2d110
Bchizi <leonbichanga@gmail.com>
2019-07-11 10:21:37 +0300

    add css, js, Radme file

---
+++ js/sript1.js
@@ -0,0 +1,5 @@
+var add = function(number1, number2){return number1 + number2;
+};
+var number1 = parseInt(prompt("weka nambari hapa:"));
+var number2 =parseInt(prompt("weka numbari hapa:"));
+alert(add(number1, number2));
