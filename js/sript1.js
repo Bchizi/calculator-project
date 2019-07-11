708705cdd5771a0d3563a8aa229836b66fccf12f
Bchizi <leonbichanga@gmail.com>
2019-07-11 11:40:17 +0300

    calculator corrections

--- js/sript1.js
+++ js/sript1.js
@@ -1,5 +1,32 @@
-var add = function(number1, number2){return number1 + number2;
+var add = function (number1, number2) {
+    return number1 + number2;
 };
 var number1 = parseInt(prompt("weka nambari hapa:"));
-var number2 =parseInt(prompt("weka numbari hapa:"));
-alert(add(number1, number2));
+var number2 = parseInt(prompt("weka numbari hapa:"));
+var result = add(number1, number2);
+alert(result)
+
+var subtract = function (number1, number2) {
+    return number1 - number2;
+};
+var number1 = parseInt(prompt("put the first number here:"));
+var number2 = parseInt(prompt("put the second number here:"));
+var result = subtract(number1, number2);
+alert(result)
+
+var multiply = function (number1, number2) {
+    return number1 * number2;
+};
+var number1 = parseInt(prompt("first number here"));
+var number2 = parseInt(prompt("second number here"));
+var result = multiply(number1, number2);
+alert(result);
+var divide = function (number1, number2) {
+    return number1 / number2;
+}
+var number1 = parseInt(prompt("first number here"));
+var number2 = parseInt(prompt("second number here"));
+var result = divide(number1, number2);
+alert(result);
+
+
